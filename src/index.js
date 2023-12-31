d4d655477dfa56092a91de2f5f67aecc8df4ebf7
Armand Bredean <armand.bredean@gmail.com>
2023-12-31 14:24:21 +0100

    remove redux

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "./sass/index.scss";
-import { Provider } from 'react-redux';
-import store from './store'
 
 import Error from './pages/error/error';
 import Home from './pages/home/home';
@@ -11,7 +9,6 @@ import Home from './pages/home/home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <Router>
         <Routes>
           <Route index element={<Home />} /> 
@@ -19,6 +16,5 @@ root.render(
           <Route path="*" element={<Error />} />
         </Routes>
       </Router>
-    </Provider>
   </React.StrictMode>
 );
